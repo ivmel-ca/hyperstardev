c72a6dd2a70069cb952a98874c95fe397a524e27
Ivan Melnychenko <ivmel.ca@gmail.com>
2020-05-03 19:30:32 +0300

    Updates

---
+++ precache-manifest.4c9f08f4af2e7aa4efbe589b6bd8f66d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7cc84cf5b985c48960446be1fadf85",
+    "url": "//Melnychenko97.github.io/hyperstardev/index.html"
+  },
+  {
+    "revision": "f5a5fc994930c34b4139",
+    "url": "//Melnychenko97.github.io/hyperstardev/static/css/main.0b9058fa.chunk.css"
+  },
+  {
+    "revision": "9be41e178e6e176ae5dd",
+    "url": "//Melnychenko97.github.io/hyperstardev/static/js/2.246c08b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//Melnychenko97.github.io/hyperstardev/static/js/2.246c08b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a5fc994930c34b4139",
+    "url": "//Melnychenko97.github.io/hyperstardev/static/js/main.971f4ee3.chunk.js"
+  },
+  {
+    "revision": "208562fc82be4551c26b",
+    "url": "//Melnychenko97.github.io/hyperstardev/static/js/runtime-main.67a55a63.js"
+  }
+]);
